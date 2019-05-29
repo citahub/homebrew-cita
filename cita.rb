8268663b4ace1b8b8bfe5eb8c563f595f959880f
clearloop <udtrokia@gmail.com>
2019-05-30 01:14:29 +0800

    slim install script

--- cita.rb
+++ cita.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # CITA Formula
 class Cita < Formula
   desc "A high performance blockchain for enterprise users."
@@ -7,18 +8,14 @@ class Cita < Formula
   url "https://github.com/cryptape/cita/releases/download/v0.24.0/cita_secp256k1_sha3.tar.gz"
   sha256 "05dd4cea809e2f10c85b1a798e636d595968d4bea4e969a7be15cf89542352c2"
   resource "cita" do
-    url "https://github.com/cryptape/cita.git",
-        :tag => "v0.24.0"
-    # :revision => ""
+    url "https://raw.githubusercontent.com/cryptape/cita/v0.24.0/env.sh"
+    sha256 "0fb0ef52141c3bdb6ffba77c86f037e98cda599c06aadd2e8a7b79799352d833"
   end
 
   def install
     libexec.install Dir["*"]
-
     resource("cita").stage do
-      system  "cp", "-r", "env.sh", "#{libexec}/bin/cita-env"
-      system  "cp", "-r", "scripts/cita", "#{libexec}/bin/cita"
-      system  "cp", "-r", "scripts/cita_config.sh", "#{libexec}/bin/cita-config"
+      system  "cp", "-r", "./env.sh", "#{libexec}/bin/cita-env"
     end
 
     bin.install_symlink Dir["#{libexec}/bin/cita"]
@@ -36,7 +33,8 @@ class Cita < Formula
          { help | create | port | setup | start | stop | restart
            ping | top | backup | clean | logs | logrotate }
      Run `cita help` for more detailed information.
-     happy hacking!
+
+     happy hacking!!! 🧙
   EOS
   end
 end
