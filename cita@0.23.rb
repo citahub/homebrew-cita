b8d1b40962f8944a98fdecc9887fdfffc9c1a756
clearloop <udtrokia@gmail.com>
2019-06-03 20:51:34 +0800

    delete verbose clone, add cita 0.23

--- cita@0.23.rb
+++ cita@0.23.rb
@@ -1,12 +1,12 @@
 # coding: utf-8
 # CITA Formula
-class Cita < Formula
+class CitaAT023 < Formula
   desc "A high performance blockchain for enterprise users."
   homepage "https://github.com/cryptape/cita"
 
-  version "0.24.0"
-  url "https://github.com/cryptape/cita/releases/download/v0.24.0/cita_secp256k1_sha3.tar.gz"
-  sha256 "05dd4cea809e2f10c85b1a798e636d595968d4bea4e969a7be15cf89542352c2"
+  version "0.23.1"
+  url "https://github.com/cryptape/cita/releases/download/v0.23.1/cita_secp256k1_sha3.tar.gz"
+  sha256 "add391c4cb8cb2f2b96e3888ae42ec3c00628821b85c4a04d05145aa825f50e4"
   
   def install
     libexec.install Dir["*"]
